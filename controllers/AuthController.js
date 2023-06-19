ac593fb4f471a3113393fd2a30b065c102976354
Actulus <schiflerpatricia@gmail.com>
2023-06-19 13:42:16 +0300

    authentification controller added needed elems

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,14 +9,26 @@ async function loginPage(_req, res, _next) {
     res.locals = {
         title: "Login",
     };
+    console.log(_req.body);
     res.render("login");
 }
 
 async function registerPage(_req, res, _next) {
     const roles = await UserModel.getRoles();
+    const departments = await UserModel.getDepartments("tanár");
+    const positions = await UserModel.getPositions("munkatárs");
+    const majors = await UserModel.getMajors("diák");
+    const years = await UserModel.getYears("diák");
+    const sections = await UserModel.getSections("adminisztrátor");
+
     res.locals = {
         title: "Register",
         roles,
+        departments,
+        positions,
+        majors,
+        years,
+        sections
     };
     res.render("register");
 }
@@ -24,11 +36,14 @@ async function registerPage(_req, res, _next) {
 // post
 async function login(_req, res, _next) {
     const { username, password } = _req.body;
+    console.log(_req.body);
     try {
         const user = await UserModel.findOne(username);
         if (!user) {
             return res.status(401).json({ message: "Invalid Credentials" });
         }
+        console.log(user.password);
+        console.log(password);
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: "Invalid Credentials" });
@@ -43,7 +58,7 @@ async function login(_req, res, _next) {
         });
 
         // redirect to home page
-        return res.redirect("/");
+        return res.redirect("/dashboard");
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal Server Error" });
@@ -51,13 +66,15 @@ async function login(_req, res, _next) {
 }
 
 async function register(req, res, _next) {
-    const { username, name, email, password, role, major, year, department, position, sections } = req.body;
+    const { username, name, email, password, passwordVerify, role, major, year, department, position, sections } = req.body;
+    console.log(req.body);
     try {
         const user = await UserModel.createUser({
             username,
             name,
             email,
             password,
+            passwordVerify,
             role,
             major,
             year,
@@ -78,7 +95,7 @@ async function register(req, res, _next) {
         });
 
         // redirect to home page
-        return res.redirect("/");
+        return res.redirect("/dashboard");
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal Server Error" });
